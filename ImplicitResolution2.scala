f1eea87e1aa9ddae219d2131018671a1816c6c6c
Thanh Le <lenguyenthanh@hotmail.com>
2024-07-05 12:25:06 +0700

    Remove scalalib dep

--- ImplicitResolution2.scala
+++ ImplicitResolution2.scala
@@ -1,15 +1,14 @@
 //> using scala 3.5.0-RC3
 // //> using options -source:3.6-migration
-//> using repository https://raw.githubusercontent.com/lichess-org/lila-maven/master
-//> using dep org.lichess::scalalib-core:11.2.3
 //> using dep org.reactivemongo::reactivemongo-bson-api:1.1.0-RC12
 
 import reactivemongo.api.bson.*
-import scalalib.newtypes.*
-import scala.util.{ Failure, NotGiven, Success, Try }
 
-opaque type RelayRoundId = String
-object RelayRoundId extends OpaqueString[RelayRoundId]
+trait SameRuntime[A, B]
+
+opaque type Id = String
+object Id:
+  given SameRuntime[Id, String] = ???
 
 trait NoDbHandler[A]
 
@@ -19,11 +18,10 @@ given [T: BSONHandler]: BSONHandler[List[T]] = ???
 given opaqueWriter[T, A](using
     rs: SameRuntime[T, A],
     writer: BSONWriter[A]
-)(using NotGiven[NoDbHandler[T]]): BSONWriter[T] with
-  def writeTry(t: T) = writer.writeTry(rs(t))
+): BSONWriter[T] = ???
 
 def write[A](a: A)(using writer: BSONWriter[A]): Unit = ???
 
-val ids: List[RelayRoundId] = ???
+val ids: List[Id] = ???
 
 val x = write(ids)
