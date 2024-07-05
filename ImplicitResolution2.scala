5e51e4b950264649218041637e3f8819307c5bb8
Thanh Le <lenguyenthanh@hotmail.com>
2024-07-05 12:58:02 +0700

    Remove reactivemongo dep

--- ImplicitResolution2.scala
+++ ImplicitResolution2.scala
@@ -1,18 +1,15 @@
 //> using scala 3.5.0-RC3
-// //> using options -source:3.6-migration
-//> using dep org.reactivemongo::reactivemongo-bson-api:1.1.0-RC12
-
-import reactivemongo.api.bson.*
+//> using options -Xprint:typer
 
 trait SameRuntime[A, B]
+trait BSONWriter[T]
+trait BSONHandler[T] extends BSONWriter[T]
 
 opaque type Id = String
 object Id:
   given SameRuntime[Id, String] = ???
 
-trait NoDbHandler[A]
-
-given BSONHandler[String] = ???
+given BSONHandler[String]                    = ???
 given [T: BSONHandler]: BSONHandler[List[T]] = ???
 
 given opaqueWriter[T, A](using
